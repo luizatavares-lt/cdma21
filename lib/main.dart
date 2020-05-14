caba09414557acb7dd8b58d096ca465eaf47e10a
Luiza de Avila Tavares <luiza.tavares@ntconsult.com.br>
2020-05-14 20:08:31 -0300

    first version

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Computador'];
+  
+  List<String> _tarefas = ['Estudar'];
+  String tarefa;
 
   @override
   Widget build(BuildContext context) {
@@ -19,37 +21,94 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(title: Center(child: Text('CDM - Tarefa A2.1'))),
         body: Column(children: [
           Container(
-              margin: EdgeInsets.all(20.0),
-              child: Center(
-                child: RaisedButton(
-                    onPressed: () {
-                      setState(() {
-                        _products.add('Mouse');
-                      });
+            margin: EdgeInsets.all(20.0),
+            child: Center(
+              child: TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: "Título da tarefa:",
+                      hintText: "Insira o título da tarefa",
+                    ),
+                    onChanged: (valor) {
+                      tarefa = valor;
                     },
-                    child: Text(
-                      'Olá Mundo!',
-                      style: TextStyle(fontSize: 22),
-                    )),
-              )),
-          Column(
-              children: _products
-                  .map((element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              child: Text(
-                                element,
-                                style: TextStyle(fontSize: 18),
-                              ),
-                              padding: EdgeInsets.all(5.0),
-                            )
-                          ],
-                        ),
-                      ))
-                  .toList()),
-        ]),
+                  )
+            )
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: Center(
+              child: RaisedButton(
+                  onPressed: () {
+                      setState(() {
+                      _tarefas.add(tarefa);
+                    });
+                  },
+                  child: Text(
+                    'Adicionar',
+                    style: TextStyle(fontSize: 22),
+                  )),
+            )),
+            Column(
+                children: _tarefas
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                child: Text(
+                                  element,
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                padding: EdgeInsets.all(5.0),
+                              )
+                            ],
+                          ),
+                        ))
+                    .toList()),
+          ]),
       ),
     );
   }
+
+  String _tarefa;
+
+  void _addTarefa(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Adicionar Tarefa"),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => 
+                setState(() {
+                  _tarefas.add(_tarefa);
+                }),
+              child: Text("Incluir"),
+            ),
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Cancelar"),
+            ),
+          ],
+          content: Form(
+            child: Column(children: <Widget>[
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: "Título da tarefa:",
+                  hintText: "Insira o título da tarefa",
+                ),
+                onChanged: (valor) {
+                  _tarefa = valor;
+                },
+              )
+            ],),
+          ) 
+        );
+      },
+    );
+  }
 }
+
+
